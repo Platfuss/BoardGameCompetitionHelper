e7ff85efe8f6b75e5ffbdd71b7305af404398464
Platfuss <michal.stanislaw.drelich@gmail.com>
2024-04-09 19:37:04 +0200

    Painting results

--- Main/Data/DTO/FoundItem.cs
+++ Main/Data/DTO/FoundItem.cs
@@ -4,7 +4,7 @@ public class FoundItem
 {
     public int BggId { get; set; }
     public string Name { get; set; } = "";
-    public (int?, string?) LanguageInfo { get; set; }
+    public (int? Id, string? Value) LanguageInfo { get; set; }
     public (int X, int Y) StartingPosition { get; set; }
     public int Length => Name.Length;
     public Directions Direction { get; set; }
